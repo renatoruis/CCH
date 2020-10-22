916fdd0dc00bbc3529692aa78cdab1f1978ac10b
Renato Ruis <renatoruis@MacBook-Pro-de-Renato.local>
2020-10-22 15:36:35 -0300

    add stone

--- desafio_stone/outputs.tf
+++
@@ -1,8 +0,0 @@
-resource "null_resource" "update" {
-  depends_on = [
-    module.eks-cluster
-  ]
-  provisioner "local-exec" {
-    command = "AWS_DEFAULT_REGION=${var.aws-region} aws eks update-kubeconfig --name ${var.cluster-name} --kubeconfig kubeconfig-stone.yaml"
-  }
-}
